32eee62e5c6e6c920614024c8780c0642f6f9b62
6-6-Syj <magand.jessy@outlook.fr>
2025-06-07 18:39:22 +0200

    FIX: cd full OK ?

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "builtins.h"
+#include "errno.h"
 #include "libft.h"
 
 /*
@@ -23,11 +24,25 @@
 	● ENOTDIR (20): Part of the specified path is not a directory.
 */
 
-static void	error_message(char *path)
+static int	w_chdir(char *path, t_data *data)
 {
-	ft_putstr_fd("cd: ", STDERR_FILENO);
-	ft_putstr_fd(path, STDERR_FILENO);
-	ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
+	if (chdir(path) == -1)
+	{
+		ft_putstr_fd("cd: ", STDERR_FILENO);
+		ft_putstr_fd(path, STDERR_FILENO);
+		if (errno == EACCES)
+			ft_putstr_fd(": Permission denied\n", STDERR_FILENO);
+		else if (errno == ENOENT)
+			ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
+		else if (errno == ENOTDIR)
+			ft_putstr_fd(": Not a directory\n", STDERR_FILENO);
+		else
+			ft_putstr_fd(": Cannot access directory\n", STDERR_FILENO);
+		data->err = errno;
+		return (data->err);
+	}
+	data->err = 0;
+	return (0);
 }
 
 static int	cd_oldpwd(t_data *data)
@@ -39,13 +54,11 @@ static int	cd_oldpwd(t_data *data)
 	{
 		if (!ft_strcmp(current->key, "OLDPWD"))
 		{
-			if (chdir(current->value) == -1)
+			if (w_chdir(current->value, data) == 0)
 			{
-				error_message(current->value);
-				data->err = 1;
+				ft_putstr_fd(current->value, STDIN_FILENO);
+				ft_putstr_fd("\n", STDIN_FILENO);
 			}
-			else
-				data->err = 0;
 			return (data->err);
 		}
 		current = current->next;
@@ -63,16 +76,7 @@ static int	cd_home(t_data *data)
 	while (current)
 	{
 		if (!ft_strcmp(current->key, "HOME"))
-		{
-			if (chdir(current->value) == -1)
-			{
-				error_message(current->value);
-				data->err = 1;
-			}
-			else
-				data->err = 0;
-			return (data->err);
-		}
+			return (w_chdir(current->value, data));
 		current = current->next;
 	}
 	ft_putstr_fd("cd: HOME not set\n", STDERR_FILENO);
@@ -97,12 +101,5 @@ int	ft_cd(t_command *cmd, t_data *data)
 	}
 	if (!ft_strcmp(cmd->args[1], "-"))
 		return (cd_oldpwd(data));
-	if (chdir(cmd->args[1]) == -1)
-	{
-		error_message(cmd->args[1]);
-		data->err = 1;
-		return (data->err);
-	}
-	data->err = 0;
-	return (0);
+	return (w_chdir(cmd->args[1], data));
 }
